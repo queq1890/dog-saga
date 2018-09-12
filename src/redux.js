e8ee4291645b2f17acf267c4e270847688d2ae6d
queq1890 <32263803+queq1890@users.noreply.github.com>
2018-09-12 17:29:16 +0700

    bow wow

--- src/redux.js
+++ src/redux.js
@@ -2,7 +2,7 @@ const API_CALL_REQUEST = "API_CALL_REQUEST";
 const API_CALL_SUCCESS = "API_CALL_SUCCESS";
 const API_CALL_FAILURE = "API_CALL_FAILURE";
 
-const initialSTate = {
+const initialState = {
   fetching: false,
   dog: null,
   error: null
